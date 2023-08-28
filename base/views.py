1190ceb6040c6ead88f5669b868d022d7063674f
Marcelo Nieva <marcelonieva7@gmail.com>
2023-08-27 21:28:15 -0300

    feat(Base): create views

--- base/views.py
+++ base/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(req):
+  return HttpResponse("HOME VIEW")
+
+def room(req):
+  return HttpResponse("ROOM VIEW")
