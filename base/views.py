a313081cb7012b87a24be118e838a8757db51982
Marcelo Nieva <marcelonieva7@gmail.com>
2023-08-27 23:39:30 -0300

    ref(Base): move templates

--- base/views.py
+++ base/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(req):
-  return render(req, 'home.html')
+  return render(req, 'base/home.html')
 
 def room(req):
-  return render(req, 'room.html')
+  return render(req, 'base/room.html')
