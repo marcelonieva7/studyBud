28c9b15e91ded47848ad221b3fe78b0a337c8c9c
Marcelo Nieva <marcelonieva7@gmail.com>
2023-08-27 21:55:47 -0300

    feat(Base): add templates to views

--- base/views.py
+++ base/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(req):
-  return HttpResponse("HOME VIEW")
+  return render(req, 'home.html')
 
 def room(req):
-  return HttpResponse("ROOM VIEW")
+  return render(req, 'room.html')
