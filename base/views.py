c258460c568cafdca4990b89affcce5567a12b2a
Marcelo Nieva <marcelonieva7@gmail.com>
2023-08-30 18:50:15 -0300

    feat(Base): connect views to database

--- base/views.py
+++ base/views.py
@@ -1,30 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-ROOMS = [
-  {
-    "id": 1,
-    "name": 'room 01',
-  },
-  {
-    "id": 2,
-    "name": 'room 02',
-  },
-  {
-    "id": 3,
-    "name": 'room 03',
-  }
-]
+from .models import Room
 
 def home(req):
-  return render(req, 'base/home.html', {"rooms": ROOMS})
+  rooms = Room.objects.all()
+  return render(req, 'base/home.html', {"rooms": rooms})
 
 def room(req, pk):
-  context = {
-    'room': None
-  }
-  for r in ROOMS:
-    if pk == r["id"]:
-      context["room"] = r
-
-  return render(req, 'base/room.html', context=context)
+  room = Room.objects.get(id=pk)
+  return render(req, 'base/room.html', context={'room': room})
