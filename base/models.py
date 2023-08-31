5bfcd1c9691df02e71f6514a9f0018676e1b23f0
Marcelo Nieva <marcelonieva7@gmail.com>
2023-08-31 16:15:19 -0300

    feat(Base): add Message Model

--- base/models.py
+++ base/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Room(models.Model):
   name = models.CharField(max_length=200)
@@ -6,6 +7,15 @@ class Room(models.Model):
   updated = models.DateTimeField(auto_now=True)
   created = models.DateTimeField(auto_now_add=True)
 
-def __str__(self):
-  return self.name
-  
+  def __str__(self):
+    return self.name
+
+class Message(models.Model):
+  user = models.ForeignKey(User(), on_delete=models.CASCADE)
+  room = models.ForeignKey(Room, on_delete=models.CASCADE)
+  body = models.TextField()
+  updated = models.DateTimeField(auto_now=True)
+  created = models.DateTimeField(auto_now_add=True)
+
+  def __str__(self):
+    return self.body[0:10]
