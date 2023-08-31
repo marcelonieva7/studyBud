b4b148039d0d51685ba3c75cad0fe1a9a1aae50a
Marcelo Nieva <marcelonieva7@gmail.com>
2023-08-31 16:36:02 -0300

    feat(Base): register new models on admin

--- base/admin.py
+++ base/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from . import models
+from .models import Room, Message, Topic
 
-admin.site.register(models.Room)
+admin.site.register(Room)
+admin.site.register(Message)
+admin.site.register(Topic)
