67d33508c5c876917307694157387c69f744ecf2
Marcelo Nieva <marcelonieva7@gmail.com>
2023-08-31 21:41:36 -0300

    feat(Base): add delete-room url

--- base/urls.py
+++ base/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
   path('room/<int:pk>', views.room, name='room'),
   path('create-room', views.createRoom, name='create-room'),
   path('update-room/<int:pk>', views.updateRoom, name='update-room'),
+  path('delete-room/<int:pk>', views.deleteRoom, name='delete-room'),
 ]
