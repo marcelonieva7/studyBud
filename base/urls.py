2e8ac9de0b0be9e7aa9aa2ce42870863a623caad
Marcelo Nieva <marcelonieva7@gmail.com>
2023-09-02 22:38:09 -0300

    feat(Base): add login route

--- base/urls.py
+++ base/urls.py
@@ -5,8 +5,11 @@ from . import views
 app_name = 'base'
 
 urlpatterns = [
+  path('login/', views.loginPage, name='login'),
+
   path('', views.home, name='home'),
   path('room/<int:pk>', views.room, name='room'),
+  
   path('create-room', views.createRoom, name='create-room'),
   path('update-room/<int:pk>', views.updateRoom, name='update-room'),
   path('delete-room/<int:pk>', views.deleteRoom, name='delete-room'),
