872967ffb98a1093617f6c99e14ec719bdfee0a7
Marcelo Nieva <marcelonieva7@gmail.com>
2023-09-05 22:31:11 -0300

    feat(Base): create comments form

--- base/forms.py
+++ base/forms.py
@@ -1,8 +1,19 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, Textarea
 
-from.models import Room
+from.models import Room, Message
 
 class RoomForm(ModelForm):
   class Meta:
     model =  Room
     fields = '__all__'
+
+class CommentForm(ModelForm):
+  class Meta:
+    model = Message
+    fields = ['body']
+    widgets = {
+      'body': Textarea(attrs={'placeholder': 'Escribe tu comentario....'})
+    }
+    labels = {
+      'body': ''
+    }
