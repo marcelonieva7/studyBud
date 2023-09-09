ef849a2f539a86bf1623e3cb1361f174dae2e43b
Marcelo Nieva <marcelonieva7@gmail.com>
2023-09-08 23:31:17 -0300

    ref(Base): fix Msg.user FK

--- base/models.py
+++ base/models.py
@@ -23,7 +23,7 @@ class Room(models.Model):
     return self.name
 
 class Message(models.Model):
-  user = models.ForeignKey(User(), on_delete=models.CASCADE)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
   room = models.ForeignKey(Room, on_delete=models.CASCADE)
   body = models.TextField()
   updated = models.DateTimeField(auto_now=True)
